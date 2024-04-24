74e8795da3ac94a7df0e589d7c53191fc53ba2f6
Jérôme Bousquié <jerome.bousquie@iut-rodez.fr>
2024-04-24 11:49:54 +0200

    ajout seuil + notify + fix move sur deltas

--- src/lib/inputs.svelte
+++ src/lib/inputs.svelte
@@ -5,32 +5,41 @@
     let pair: string = 'BTC/USD';
     let buyPrice: number = 65000;
     let stake: number = 100;
+    let threshold:  number = 100;
     let feeRates = [0.1, 0.12, 0.14, 0.2, 0.22, 0.24, 0.25, 0.35, 0.4];
-    let pairs = ['BTC/USD', 'BTC/EUR', 'ETH/EUR'];
+    let pairs = ['BTC/USD', 'BTC/EUR', 'ETH/EUR', 'ETC/EUR', 'BTC/USDT'];
+    $: splitPair = pair.split('/');
+    $: crypto = splitPair[0];
+    $: currency = splitPair[1];
 
 </script>
 
-<label for="feeRates">Frais </label>
+
+
+<label for="pairs">Paire = </label>
+<select id="pairs" name="pairs" bind:value={pair}>
+    {#each pairs as p }
+        <option value={p}>&nbsp;{p}</option>
+    {/each}
+</select>
+<br/>
+<label for="feeRates">Frais = </label>
 <select id="feeRates" name="feeRates"  bind:value={feeRate}>
     {#each feeRates as f }
         <option value={f}>{f} %</option>
     {/each}
 </select>
-
-<label for="pairs">Paire </label>
-<select id="pairs" name="pairs" bind:value={pair}>
-    {#each pairs as p }
-        <option value={p}>{p}</option>
-    {/each}
-</select>
 <br/>
 <br/>
-<label for="price">Prix d'achat </label>
-<input type="number" id="price" name="price" bind:value={buyPrice} min="0">
-
-<label for="stake">Mise </label>
-<input type="number" id="stake" name="stake" bind:value={stake} min="0">
+<label for="price">Prix d'achat {crypto} en {currency} = </label>
+<input type="number" id="price" name="price" bind:value={buyPrice} min="0"> 
+<br/>
+<label for="stake">Mise en {currency} = </label>
+<input type="number" id="stake" name="stake" bind:value={stake} min="0"> 
+<br/>
+<label for="threshold">Seuil de gain en {currency} = </label>
+<input type="number" id="threshold" name="threshold" bind:value={threshold} min="0"> 
 <br/>
 <div id="data">
-    <Trades {feeRate} {pair} {buyPrice} {stake} {pairs} />
+    <Trades {feeRate} {pair} {buyPrice} {stake} {pairs} {currency} {crypto} {threshold} />
 </div>
--- src/lib/trades.svelte
+++ src/lib/trades.svelte
@@ -2,29 +2,34 @@
 
 <script lang="ts">
     // https://docs.kraken.com/websockets-v2/#trade
-    import Trades from "$lib/trades.svelte";
     import { onMount } from "svelte";
 
     export let feeRate: number = 0.1;
-    export let pair: string = 'BTC/USD';
+    export let pair: string;
     export let buyPrice: number = 1000;
     export let stake: number = 100;
-    export let pairs: [string];
+    export let pairs: any;
+    export let crypto: string;
+    export let currency: string;
+    export let threshold: number = 100;
     
-    $: currency = pair.split('/')[1];
+
     $: entryFee = feeRate * stake * 0.01;
     $: sellPrice = 100000;
     $: variation = (sellPrice - buyPrice) / buyPrice;
     $: grossValue = (variation + 1) * (stake - entryFee);
     $: exitFee = feeRate * grossValue * 0.01;
     $: netGain = grossValue - stake - exitFee;
-    $: plus = (netGain >= 0) ? '+' : '';
+    $: plusVariation = (variation >= 0) ? '+' : '';
+    $: plusGain = (netGain >= 0) ? '+' : '';
     $: sign = (netGain >= 0) ? 'pos' : 'neg';
+    $: checkGainThreshold();
 
-
-    let connecte = false;
+    let maxTradeNb: number = 25;
+    let connecte: boolean = false;
+    let title: string = 'SimuKraken';
     let trades: [{symbol: string; side: string; price: number; qty: number; ord_type: string}];
-    let tradeList = '';
+    let tradeList: string = '';
 
     // derniers prix : on tient à jour un tableau de 100 entrées par paire
     let lastPrices: any = {};
@@ -33,17 +38,47 @@
         lastPrices[pr] = [];
     }
 
+    // test si gain >= seuil
+    function checkGainThreshold() {
+        if (netGain >= threshold) {
+            notifyThreshold();
+        } else {
+            notifyReset();
+        }
+    }
+    // notifie à chaque fois que le gain passe au dessus du seuil
+    function notifyThreshold() {
+        let msgGain = "Gain : " + netGain.toFixed(2) + " " + currency + " !!!"
+        document.title = (document.title == title) ? msgGain : title;       
+    }
+
+    // supprime la notification de seuil dépassé
+    function notifyReset() {
+        document.title = title;
+    }
+
+
     // retourne une string html des trades dans l'ordre chrono (inversé)
     function listTrades(lastPricePair: any): string {
         let listTrade = '<br/>';
         for (let i = lastPricePair.length - 1; i >= 0; i--) {
             let trd = lastPricePair[i];
             let line = trd.line;                                
-            let color: string = (trd.side == "sell") ? "red;" : 'green;';
-            let style = "style=\"color:"+ color + "\";";
-            listTrade = listTrade + "<span " + style +">" + line + "</span><br/>";
+            let cls = (trd.side == "sell") ? "sell" : 'buy';
+            listTrade = listTrade + "<span class=" + cls +">" + line + "</span><br/>";
         }
         return listTrade;
+    }
+
+    // fonction qui renvoie le  caractère ⮝ ou ⮟  ou "&nbsp;" selon le delta curent - last
+    function getMove(last: number, current: number): string {
+        let move = "&nbsp;";
+        if (current - last > 0) {
+            move = "⮝";
+        } else if (last - current > 0) {
+            move = "⮟";
+        }
+        return move;
     }
 
     onMount(() => {
@@ -92,10 +127,26 @@
                         // on stoke les nouvelles lignes de trade dans le tableau de la paire
                         for (let i = 0; i < trades.length; i++) {
                             let trd = trades[i];
-                            let line: string = trd.symbol + " ordre = " + trd.side + " prix = " + trd.price + " " + currency + " qty = " + trd.qty + " type ordre = " + trd.ord_type;
-                            let elem = {price: trd.price,  side: trd.side, line: line};
+                            let prev;
+                            if (i > 0) {                               
+                                prev = trades[i  - 1];          // on compare les prix au sein des trades reçus
+                            } else {
+                                let l = lastPrices.length;
+                                if (l > 0) {
+                                    prev = lastPrices[l - 1]; // on compare le premier trade reçu à celui reçu précédemment
+                                } else {
+                                    prev = {price: trd.price};
+                                }
+                            }
+                            let move = getMove(prev.price, trd.price);
+
+                            let sd = (trd.side == "buy") ? "buy " : trd.side;
+                            let tp = (trd.ord_type == "limit") ? " limit" : trd.ord_type;
+
+                            let line: string = trd.symbol + " " + sd + "/" + tp + "&nbsp;&nbsp;&nbsp;<b>" + trd.price.toFixed(2) + " " + move +"</b>&nbsp;&nbsp;&nbsp;" + trd.qty.toFixed(4) ;
+                            let elem = { price: trd.price,  side: trd.side, line: line };
                             lastPrices[tradePair].push(elem);
-                            if (lastPrices[tradePair].length >= 100) {
+                            if (lastPrices[tradePair].length >= maxTradeNb) {
                                 lastPrices[tradePair].shift();
                             }
                         }
@@ -114,6 +165,8 @@
                         let lastPricePair = lastPrices[pair];
                         let lastIndex = lastPricePair.length - 1;
                         sellPrice = lastPricePair[lastIndex].price;
+                        tradeList = listTrades(lastPricePair);
+                        checkGainThreshold();
                         break;
                 }
             }
@@ -126,27 +179,25 @@
 
     });
 </script>
-
-
-
-<div id="status">
-    {#if connecte } 
-        <span>Connecté à Kraken</span>
-    {/if}
-</div>
+{#if connecte}
+<br/>
+ <div class="buy">Connecté à Kraken</div>
+{/if}
 <br/>
 <div>
     frais à l'achat : {entryFee.toFixed(2)} {currency}<br/>
-    prix de la cryto : {sellPrice.toFixed(2)} {currency}<br/>
-    variation du prix par rapport à l'achat : {variation.toFixed(3)} %<br/>
+    prix de la crypto {crypto} : <b>{sellPrice.toFixed(2)} {currency}</b><br/>
+    variation du prix par rapport à l'achat : {plusVariation}{variation.toFixed(3)} %<br/>
     valeur actuelle de la mise : <b>{grossValue.toFixed(2)} {currency}</b><br/>
     frais de revente : {exitFee.toFixed(2)} {currency} <br/>
-    <b>gain net : <span class={sign}>{plus}{netGain.toFixed(2)} {currency}</span></b><br/>
+    <b>gain net : <span class={sign}>{plusGain}{netGain.toFixed(2)} {currency}</span></b><br/>
 </div>
 <br/>
 <div>
     <u>TRADES LIVE</u>
+    <div class="list">
         {@html tradeList}
+    </div>
 </div>
 
 <style>
@@ -155,5 +206,9 @@
     }
     .neg {
         color: red;
+    }
+    .list {
+        font-family: 'Courier New', Courier, monospace;
+        font-size: 15px;
     }
 </style>
